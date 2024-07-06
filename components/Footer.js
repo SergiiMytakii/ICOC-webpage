33f7bf23a3b12dd01feae19c178f6f29573d3f0b
Sergii <serjmitaki@gmail.com>
2024-07-06 11:01:30 +0300

    page content

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,32 @@
 import styles from './Footer.module.css'
+import { FaEnvelope, FaTelegram, FaFacebook } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
-    </>
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <p>App made by Sergii Mytakii from Odesa, Ukraine</p>
+        <div className={styles.contactsContainer}>
+          <span className={styles.iconContainer}>
+            <FaEnvelope className={styles.icon} />
+            <a href="mailto:serjmitaki@gmail.com" target="_blank" rel="noopener noreferrer">
+              serjmitaki@gmail.com
+            </a>
+          </span>
+          <span className={styles.iconContainer}>
+            <FaTelegram className={styles.icon} />
+            <a href="https://t.me/serjmitaki" target="_blank" rel="noopener noreferrer">
+             Telegram
+            </a>
+          </span>
+          <span className={styles.iconContainer}>
+            <FaFacebook className={styles.icon} />
+            <a href="https://www.facebook.com/serjmitaki" target="_blank" rel="noopener noreferrer">
+              Facebook
+            </a>
+          </span>
+        </div>
+      </div>
+    </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,51 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>ICOC</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="International Churches of Christ" />
+        <meta property="og:title" content="ICOC" />
+        <meta property="og:description" content="Open in the app" />
+        <meta property="og:image" content="/icoclogo.jpg" />
+        <meta property="og:url" content="https://icoc.netlify.app" />
+
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to ICOC app!" />
+        <img src="/icoclogo.jpg" alt="ICOC Logo" />
+        <div className="content-container">
+          <p>Now there's no need to carry paper literature, all necessary materials are in the app.</p>
+
+          <ul>
+            <li>Christian songs with chords and videos</li>
+            <li>Basics of Christianity</li>
+            <li>Q&A</li>
+            <li>Video teachings</li>
+          </ul>
+        </div>
+
+        <h2 className="description">
+          Do not have the app?
+        </h2>
+        <h2>
+          Download it here:
+        </h2>
+        <div className="image-container">
+          <a href="https://play.google.com/store/apps/details?id=ru.icoc.app" target="_blank" rel="noopener noreferrer">
+            <Image src="/512x512.png" alt="Google Play Market" width={150} height={50} layout="responsive" />
+          </a>
+
+          <a href="https://apps.apple.com/us/app/icoc/id1585486521" target="_blank" rel="noopener noreferrer">
+            <Image src="/appstore.png" alt="App Store" width={150} height={50} layout="responsive" />
+          </a>
+        </div>
       </main>
 
       <Footer />
